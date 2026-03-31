b0ca47f59d5dd3ba7ae48ec76638e62c13686097
jing.weiye <jing.weiye@zhaopin.com.cn>
2026-03-31 15:38:24 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,6 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-	   server: {
-        port: 3000,
-        host: '0.0.0.0',
-		allowedHosts: [
-		'kefu-400.onrender.com',
-          'four00-kefu.onrender.com',
-          '.onrender.com' // 允许所有 onrender.com 子域名
-        ]
-      },
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
@@ -28,6 +19,13 @@ export default defineConfig(({mode}) => {
       // HMR is disabled in AI Studio via DISABLE_HMR env var.
       // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
       hmr: process.env.DISABLE_HMR !== 'true',
+      port: 3000,
+        host: '0.0.0.0',
+		allowedHosts: [
+		'kefu-400.onrender.com',
+          'four00-kefu.onrender.com',
+          '.onrender.com' // 允许所有 onrender.com 子域名
+        ]
     },
   };
 });
